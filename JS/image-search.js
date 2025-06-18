73ec800c8b88d0b02468dd0c9a05281e6b1178c7
Lilian-UXR <llim712@gmail.com>
2025-06-18 12:27:49 -0700

    Initial Start to Project
    html, css, sass, and javascript files created; added styling to css pages and fonts to html; javascript files still in rough shape

---
+++ JS/image-search.js
@@ -0,0 +1,25 @@
+let SearchContainer = document.querySelector('.js-search-container');
+let form = document.querySelector('.js-search-form');
+let SearchInput = document.querySelector('[name=image-search-input]');
+
+const imageBeingSearched = `&q=${SearchInput.value}`;
+const apikey = 'api_key=ihLC7ZyuQR0EdKRSflFGUrR3mv3tPAYn';
+const url = `https://api.giphy.com/v1/stickers/search?`
+const bounds = '&limit=25&rating=pg&lang=en'
+
+// user input search item for images
+// get image search item
+// javascript needs image search item to connect to api url
+// generate image gifs
+
+async function getData() {
+    let ImageUrl = url + apikey + imageBeingSearched + bounds;
+    try {
+        const response = await fetch(ImageUrl);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
