5dda66351dd5810b77a29799ef62b4353bed3de1
Lilian-UXR <llim712@gmail.com>
2025-06-19 14:04:23 -0700

    Update to HTML and JS
    getting links to work, so api url can now be linked and shown in console.log

--- JS/image-search.js
+++ JS/image-search.js
@@ -1,29 +1,35 @@
-const SearchInput = document.getElementById('image-search-input');
+const SearchInput = document.getElementById("image-search-input");
 
-let SearchContainer = document.querySelector('.js-search-container'); //where images should populate
-let form = document.querySelector('.js-search-form');
+let SearchContainer = document.querySelector(".js-search-container"); //where images should populate
+let form = document.querySelector(".js-search-form");
 
-const imageBeingSearched = `&q=${SearchInput.value}`;
-const apikey = 'api_key=ihLC7ZyuQR0EdKRSflFGUrR3mv3tPAYn'; //note this is old key
-const url = `https://api.giphy.com/v1/stickers/search?`
-const bounds = '&limit=25&rating=pg&lang=en'
+const apikey = "api_key=insertapikeylater"; //need key insert for it to work
+const url = `https://api.giphy.com/v1/stickers/search?`;
+const bounds = "&limit=25&rating=pg&lang=en";
 
+// how to use secret api key and keep it a secret?
 // user input search item for images in HTML form
 // use input in HTML is linked in Javascript
 // javascript needs image search item to connect to api url
 // api url links need to generate and render HTML to display on page (innerHTML)
 
 async function getData() {
-    let ImageUrl = url + apikey + imageBeingSearched + bounds;
-    try {
-        const response = await fetch(ImageUrl);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`)
-        }
-        const blob = await response.blob();
-        const objectURL = ImageUrl.createObjectURL(blob);
-        image.src = objectURL;
-    } catch (error) {
-        console.error(error.message);
+  const imageBeingSearched = `&q=${SearchInput.value}`;
+  let ImageUrl = url + apikey + imageBeingSearched + bounds;
+  console.log(ImageUrl);
+  try {
+    const response = await fetch(ImageUrl);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getData();
+});
