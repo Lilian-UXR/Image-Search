3829b3eb775b62f58c78818a5289e0a40be8ecd3
Lilian-UXR <llim712@gmail.com>
2025-06-19 14:46:07 -0700

    Update to html and js
    update name of containers and added code to attempt to produce html code for gif page

--- JS/image-search.js
+++ JS/image-search.js
@@ -1,17 +1,18 @@
 const SearchInput = document.getElementById("image-search-input");
 
-let SearchContainer = document.querySelector(".js-search-container"); //where images should populate
+const ImageContainer = document.querySelector(".js-image-container"); //where images should populate
 let form = document.querySelector(".js-search-form");
 
-const apikey = "api_key=insertapikeylater"; //need key insert for it to work
+const apikey = "api_key=inputkey"; //need key insert for it to work
 const url = `https://api.giphy.com/v1/stickers/search?`;
 const bounds = "&limit=25&rating=pg&lang=en";
 
-// how to use secret api key and keep it a secret?
-// user input search item for images in HTML form
-// use input in HTML is linked in Javascript
-// javascript needs image search item to connect to api url
-// api url links need to generate and render HTML to display on page (innerHTML)
+// [ ] how to use secret api key and keep it a secret?
+// [X] user input search item for images in HTML form
+// [X] user input in HTML is linked in Javascript
+// [X] javascript needs image search item to connect to api url
+// [ ] api url needs to link to gif associated with search
+// [ ] gif url need to generate and render HTML to display on page (innerHTML)
 
 async function getData() {
   const imageBeingSearched = `&q=${SearchInput.value}`;
@@ -33,3 +34,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   getData();
 });
+
+async function loadImages(SearchInput) {
+  ImageContainer.innerHTML = "";
+  if (SearchInput === null) {
+    await getData();
+  }
+  return `
+        <div class="images"> 
+            <img src="${stickers.data.images.orignial.url}" 
+            alt="${SearchInput.value} gif" />
+        </div>
+    `;
+}
